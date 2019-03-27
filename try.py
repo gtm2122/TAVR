f9d9f1e4714f61b785c347117ed4028f3e2ebc94
gtm2122 <gabrielthomas281@gmail.com>
2019-03-27 09:53:37 -0400

    bug contiguous

--- try.py
+++ try.py
@@ -15,7 +15,13 @@ import matplotlib.pyplot as plt
 import pickle
  
 
-
+def conv_block_3d(in_dim,out_dim,act_fn):
+    model = nn.Sequential(
+        nn.Conv3d(in_dim,out_dim, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm3d(out_dim),
+        act_fn,
+    )
+    return model
 
 image = torch.zeros((1,1,256,256,256))
 mask = torch.zeros((1,1,256,256,256))
@@ -28,24 +34,17 @@ image, mask = Variable(image), Variable(mask)
 #print(image.size())
 print(image.size())
 print(mask.size())
-#model  = UnetGenerator_3d(in_dim=1,out_dim=2,num_filter=1,crop=False,smax=False).cuda()
 
 in_dim = 1
 out_dim = 2
 act_fn = nn.ReLU(inplace=True)
-model = nn.Sequential(
-        conv_block_3d(in_dim,out_dim,act_fn),
-        nn.Conv3d(out_dim,out_dim, kernel_size=3, stride=1, padding=1),
-        nn.BatchNorm3d(out_dim),
-    ).cuda()
+
+model = nn.Sequential(conv_block_3d(in_dim,out_dim,act_fn)).cuda()
+
 optimizer = optim.SGD(model.parameters(),lr=0.1)
 optimizer.zero_grad()
 
 output = model(image.contiguous())
-#print(image.size())
-#         print(output.size())
-#         print(mask.size())
-
 
 
 criterion = torch.nn.L1Loss()#DICELossMultiClass()
@@ -54,21 +53,10 @@ optimizer.zero_grad()
 
 output = model(image.contiguous())
 print(output.size())
-#print(image.size())
-#         print(output.size())
-#         print(mask.size())
-#         print(output.size())
-#         print(mask.size())
 
 loss = criterion(output.contiguous(), mask.contiguous())
-#         print(loss.size())
-#         print(loss)
+
 loss = loss.contiguous()
-# e_loss+=loss.item()
-#print(len(train_loader))
-#         print(c)
-# if c in l:
-#     print(100*c/len(train_loader))
 
 loss.backward()
 optimizer.step()
